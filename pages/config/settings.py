234ca2a96ae1e515ee6c9177a748fedcc78b6a07
Sipo Charles <sipocharles@yahoo.co.uk>
2020-10-02 03:46:14 +0100

    New updates for Heroku deployment

--- pages/config/settings.py
+++ pages/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o^6v#hb)r+2t@$r3%cotiu+#m4vpejosbm@+$4vc!7dm3c8fu#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
