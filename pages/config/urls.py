5c2a229fb955176a83445482ac57700d38cab024
Sipo Charles <sipocharles@yahoo.co.uk>
2020-10-02 20:34:34 +0100

    config & urls.py

--- pages/config/urls.py
+++ pages/config/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# config/urls.py
+# config/urls.py #
 from django.contrib import admin
-from django.urls import path, include # new
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')), # new
+    path('', include('pages.urls')),
 ]
